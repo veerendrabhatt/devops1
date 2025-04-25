e1472dcb2cba8c3fc90b46c034eb58f5e610dc6a
veerendrabhatt <veerendrap.22cse@cambridge.edu.in>
2025-04-25 11:40:00 +0530

    Initial commit

---
+++ src/main/java/com/CIT/App.java
@@ -0,0 +1,13 @@
+package com.CIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! i am from mavne " );
+    }
+}
